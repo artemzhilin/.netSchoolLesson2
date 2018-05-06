452b1ec9d5a321920cb33d430ab80bb264101706
Zhilin <artemzhilin@gmail.com>
2018-05-06 18:24:53 +0300

    task2

---
+++ Task2/BankOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Task2
+{
+    [XmlRoot("operation")]
+    public class BankOperation: IComparable<BankOperation>
+    {
+        [XmlAttribute("type")]
+        public string OperationType { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public int CompareTo(BankOperation other)
+        {
+            return Amount.CompareTo(other.Amount);
+        }
+
+        public override string ToString()
+        {
+            return $"Date: {this.Date}\nOperation type: {this.OperationType}\nAmount: {this.Amount}";
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace Task2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var filesList = Directory.GetFiles("operations");
+            var xmlOperationSerializer = new XmlSerializer(typeof(BankOperation));
+            var operations = new List<BankOperation>();
+            foreach (var filePath in filesList)
+            {
+                try
+                {
+                    using (var reader = new StreamReader(filePath))
+                    {
+                        switch (Path.GetExtension(filePath))
+                        {
+                            case ".xml":
+                                operations.Add((BankOperation)xmlOperationSerializer.Deserialize(reader));
+                                break;
+                            case ".json":
+                                operations.Add(JsonConvert.DeserializeObject<BankOperation>(reader.ReadToEnd()));
+                                break;
+                            default:
+                                Console.WriteLine($"Unexpected file extension: {filePath}");
+                                break;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Could not read file {filePath}. Maybe it's broken");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(operations.Max());
+        }
+    }
+}
